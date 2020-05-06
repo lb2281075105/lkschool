035174f6f6675eeff73f1481e3d49d381088a259
liubo <2281075105@qq.com>
2020-05-06 10:58:57 +0800

    提交

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,43 @@
 import express from 'express';
 const router = express.Router();
 
+
+/*******************后端页面路由***********************/
 router.get('/back',(req,res)=>{
 
     // render 默认会进如到views文件夹下
     res.render('back/index.html');
 });
+
+
+
+/*******************前端页面路由***********************/
+
+router.get('/',(req,res)=>{
+    res.redirect('/web');
+});
+// 会重定向到这个路由
+router.get('/web',(req,res)=>{
+    res.render('web/index.html');
+});
+
+router.get('/web/res',(req,res)=>{
+    res.render('web/resources.html');
+});
+
+
+router.get('/web/res_c',(req,res)=>{
+    res.render('web/resources_content.html');
+});
+
+
+
+
+
+
+
+
+
 
 
 // CommonJS规范
